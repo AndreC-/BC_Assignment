91a1fad519e8bdd0db3f713186ea14664dfa46dc
Andre Chalhoub <andrechalhoub@Andres-MacBook-Pro.local>
2024-11-26 10:32:29 -0500

    Updated the solution to question 2 to better handle an error response

--- questions/question_2.py
+++ questions/question_2.py
@@ -3,14 +3,16 @@ import requests
 response = requests.get(
 'http://api.openweathermap.org/data/2.5/weather',
     params={
-        "q": "Toronto", "units": "metric", "APPID": "c7345a6839c51552b824e975a3b068e9"
+        "q": "Toronto", "units": "metric", "appid": "c7345a6839c51552b824e975a3b068e9"
     }
 )
 
-assert 200 == response.status_code
-print(response.json()["main"]["temp"])
-print(response.status_code)
+if response.status_code == 200:
+    print("Current temperature in Toronto: {}".format(response.json()["main"]["temp"]))
+    print("API response status: {}".format(response.status_code))
+else:
+    print("API response status: {}. An error occurred while retrieving the data".format(response.status_code))
 
-# Output
-# 5.19
-# 200
+# Output:
+# Current temperature in Toronto: 6.41
+# API response status: 200
